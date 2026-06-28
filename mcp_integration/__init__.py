3409e9e52c39b5638847a0bd02961c141607d3ad
boris-dotv <1322553126@qq.com>
2026-06-28 19:08:46 +0000

    auto: mcp_integration/__init__.py - from .mcp_client import MCPClient, MCPLogger, MCPFunctionReg

--- mcp_integration/__init__.py
+++ mcp_integration/__init__.py
@@ -3,6 +3,6 @@
 MCP Integration - Model Context Protocol集成模块
 """
 
-from .mcp_client import MCPClient, MCPLogger, MCPFunctionRegistry
+from .mcp_client import MCPClient, MCPLogger, MCPFunctionRegistry  # noqa: F401
 
 __all__ = ["MCPClient", "MCPLogger", "MCPFunctionRegistry"]
