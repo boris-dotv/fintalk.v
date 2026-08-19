8e76b90f5fca69748223cefbb077bcd19b4a01fb
boris-dotv <1322553126@qq.com>
2026-08-19 04:30:06 +0000

    auto: mcp_integration/__init__.py - __version__ = '0.1.3'

--- mcp_integration/__init__.py
+++ mcp_integration/__init__.py
@@ -6,4 +6,4 @@ MCP Integration - Model Context Protocol集成模块
 from .mcp_client import MCPClient, MCPLogger, MCPFunctionRegistry  # noqa: F401
 
 __all__ = ["MCPClient", "MCPLogger", "MCPFunctionRegistry"]
-__version__ = "0.1.2"
+__version__ = "0.1.3"
